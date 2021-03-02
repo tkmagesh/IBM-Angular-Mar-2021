ea96dd6f7f3fd1c256550f13b7396216fecf83be
Magesh Kuppan <tkmagesh77@gmail.com>
2021-03-02 11:12:29 +0530

    ES6 Modules

--- first-app/src/main.ts
+++ first-app/src/main.ts
@@ -4,6 +4,36 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+//ES6 Modules
+/* 
+import * as calc from './calculator'
+console.log(calc) 
+*/
+
+/* 
+import * as calc from './calculator'
+console.log(calc.add(100,200)) 
+*/
+
+/* 
+import * as calc from './calculator'
+const add = calc.add
+console.log(add(100,200)) 
+*/
+
+/* 
+import * as calc from './calculator'
+//const add = calc.add
+//using object destructuring for the above line
+const { add } = calc
+console.log(add(1000,200))  
+*/
+
+/* 
+import { add, subtract } from './calculator'
+console.log(add(1000,500))   
+*/
+
 if (environment.production) {
   enableProdMode();
 }
