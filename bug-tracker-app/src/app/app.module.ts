648f4066805536d4d5f76b11956a02912e4997ca
Magesh Kuppan <tkmagesh77@gmail.com>
2021-03-03 13:12:42 +0530

    Use case 7  : Improve bug display (implementation) (creating custom pipes)

--- bug-tracker-app/src/app/app.module.ts
+++ bug-tracker-app/src/app/app.module.ts
@@ -4,10 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BugTrackerComponent } from './bug-tracker/bug-tracker.component';
 
+import { TrimTextPipe } from './bug-tracker/pipes/trimText.pipe';
+
 @NgModule({
   declarations: [
     AppComponent,
-    BugTrackerComponent
+    BugTrackerComponent,
+    TrimTextPipe
   ],
   imports: [
     BrowserModule
