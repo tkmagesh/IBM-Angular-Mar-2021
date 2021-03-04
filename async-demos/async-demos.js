5679f347a41cc9914505d95215f682b53cc6feaa
Magesh Kuppan <tkmagesh77@gmail.com>
2021-03-04 17:19:50 +0530

    Day-4 complete

--- async-demos/async-demos.js
+++ async-demos/async-demos.js
@@ -32,4 +32,52 @@
 
     window['addAsyncClient'] = addAsyncClient;
 
+    function addAsyncPromise(x,y){
+        console.log(`   [@Service] processing ${x} and ${y}`);
+
+        var p = new Promise(function(resolveFn, rejectFn){
+            setTimeout(function(){
+                var result = x + y;
+                console.log(`   [@Service] returing result`);
+                resolveFn(result);
+            },5000);
+        })
+        return p;
+    }
+
+    function addSyncClient(x,y){
+        console.log(`[@Client] invoking the service`);
+        var result = addSync(x,y);
+        console.log(` [@Client] result = ${result}`);
+    }
+
+    /* 
+    function addAsyncPromiseClient(x,y){
+        console.log(`[@client] triggering the service`);
+        var p = addAsyncPromise(x,y);
+        p.then(function(result){
+            console.log(`[@client] result =${result}`);
+        });
+    } 
+    */
+
+    async function addAsyncPromiseClient(x,y){
+        console.log(`[@client] triggering the service`);
+        var result = await addAsyncPromise(x,y);
+        console.log(`[@client] result =${result}`);
+    }
+
+    window['addAsyncPromiseClient'] = addAsyncPromiseClient;
+
+    function divide(x,y){
+        return new Promise(function(resolveFn, rejectFn){
+            setTimeout(() => {
+                if (y !== 0){
+                    return resolveFn(x / y);
+                } else {
+                    return rejectFn(new Error('Invalid arguments'));
+                }
+            }, 5000)
+        });
+    }
 })();
